3970ccdaba5f043b9db9fcfc5518ca99c6d54400
Rachit yadav <rachit.r.yadav@gmail.com>
2024-03-19 22:50:52 +0530

    rev - changes

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,27 +1,27 @@
 class Api::V1::OrdersController < ApplicationController
     before_action :authenticate_user!
 
-    def index
-        @current_user = current_user
-    end
+    # def index
+    #     @current_user = current_user
+    # end
 
-    def create
-        @current_user = current_user
-        if params[:address].present?
-            @shopping_address = current_user.addresses.new(allowed_params)
-            @shopping_address.default = true          if current_user.default_shipping_address.nil?
-            @shopping_address.billing_default = true  if current_user.default_billing_address.nil?
-            @shopping_address.save
-            @form_address = @shopping_address
-        elsif params[:shopping_address_id].present?
-            @shopping_address = current_user.addresses.find(params[:shopping_address_id])
-        end
+    # def create
+    #     @current_user = current_user
+    #     if params[:address].present?
+    #         @shopping_address = current_user.addresses.new(allowed_params)
+    #         @shopping_address.default = true          if current_user.default_shipping_address.nil?
+    #         @shopping_address.billing_default = true  if current_user.default_billing_address.nil?
+    #         @shopping_address.save
+    #         @form_address = @shopping_address
+    #     elsif params[:shopping_address_id].present?
+    #         @shopping_address = current_user.addresses.find(params[:shopping_address_id])
+    #     end
 
-    end
+    # end
 
-    private
+    # private
 
-    def allowed_params
-      params.require(:address).permit(:first_name, :last_name, :address1, :address2, :city, :state_id, :state_name, :zip_code, :default, :billing_default, :country_id)
-    end
+    # def allowed_params
+    #   params.require(:address).permit(:first_name, :last_name, :address1, :address2, :city, :state_id, :state_name, :zip_code, :default, :billing_default, :country_id)
+    # end
 end
